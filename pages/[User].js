3882fae8018ed3188e05b31150a26684857e61dd
Francisco Herrera <fherrera-guerrero@csumb.edu>
2021-12-19 22:17:58 -0800

    Base template

---
+++ pages/[User].js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+
+interface Props {
+
+}
+
+const User = (Props) => {
+
+
+  return (
+    <div>Username here:</div>
+  )
+}
+
+export async  getServerSideProps(context) {
+
+  const
+
+  return {
+    props: {}
+  }
+}
