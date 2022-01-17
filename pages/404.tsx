d6b14c61d50a34b0b4ae784febcaa0634a03751e
Lykøs <lykos@lykss-mbp.lan>
2022-01-17 13:59:35 -0800

    basic 404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Custom404() {
+  return (
+    <div className='bg-slate-900 h-screen w-screen flex flex-col justify-center items-center text-center'>
+      <h1 className='text-9xl font-black text-white'>404</h1>
+      <div className='text-6xl font-bold text-white'>Oh no it seems this content doesn't exist</div>
+    </div>
+  )
+}
