d9ee3012fccc4bf1783740b8d2d140a4887f0c21
Lykøs <lykos@lykss-mbp.lan>
2022-01-18 15:31:38 -0800

    adjusted navigation on posts

--- components/PostNavigation.tsx
+++ components/PostNavigation.tsx
@@ -1,25 +1,22 @@
 
 import { useRouter } from 'next/router'
-import Image from 'next/image'
-import NavButton from './Navigation/NavButton'
+import PostNavButton from './PostNavigation/PostNavButton'
 
 
-const PostNavigation = () => {
+const PostNavigation = (prop: any) => {
   const router = useRouter()
   const { User } = router.query
 
-//  const { data: firstArr } = useSWR(`/api/${User}/albumIds?page=${pid}`, fetcher)
-//  const { data: secondArr } = useSWR(`/api/${User}/albumIds?page=${Number(pid) + 1}`, fetcher)
-
+  console.log(prop)
 
   return (
   <div className='fixed top-0 left-0 right-0 z-10 bg-stone-900 text-white'>
     
     <nav className='w-full h-20 z-20 flex justify-center items-center text-center space-x-96'>
       {/*<Image width={80} height={80} src='/headpats.gif' className='absolute top-0 left-0 right-0'/>*/}
-      <NavButton direction={false}/>
+      <PostNavButton prevArr={prop.prevArr} prevPostion={prop.prevPosition} currPosition={prop.currPosition} currArr={prop.currArr} direction={false}/>
       <div>{User}</div>
-      <NavButton direction={true} />
+      <PostNavButton nextArr={prop.nextArr} currArr={prop.currArr} nextPosition={prop.nextPostion} direction={true}/>
     </nav>
   </div>
 )
--- pages/[User]/[pid]/[post].tsx
+++ pages/[User]/[pid]/[post].tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router'
 import useSWR from 'swr'
 import Loading from '../../../components/load'
 import Link from 'next/link'
+import PostNavigation from '../../../components/PostNavigation'
 
 
 type postType = {
@@ -29,17 +30,20 @@ const UserPost:NextPage<postType> = ({ data }) => {
 
   const { data: currArr } = useSWR(`/api/${User}/albumIds?page=${pid}`, fetcher)
   const { data: nextArr } = useSWR(`/api/${User}/albumIds?page=${Number(pid) + 1}`, fetcher)
-  const { data: prevArr } = useSWR(`/api/${User}/albumIds?page=${Number(pid) - 1}`, fetcher)
-  if(!currArr && !nextArr && !prevArr && router.isFallback) return <Loading /> 
-  const postPos = currArr.response.indexOf(post) 
-  const prevPost = postPos - 1 > 0 ? postPos - 1 : 59
+  const { data: prevArr } = useSWR(Number(pid) - 1 ? `/api/${User}/albumIds?page=${Number(pid) - 1}` : null, fetcher)
+  if(!currArr || !nextArr || (!prevArr && prevArr === null) || router.isFallback) return <Loading /> 
+  const postPos = currArr.response.indexOf(post)
+  const prevPost = postPos - 1 >= 0 ? postPos - 1 : 59
   const nextPost = postPos + 1 > 59 ? 0 : postPos + 1
 
 
   return (
-    <div className='bg-slate-800 pt-12'>
-      <Post data={data} />
-    </div>
+    <>
+      <PostNavigation currPosition={postPos} prevPosition={prevPost} prevArr={prevArr?.response} nextPostion={nextPost} currArr={currArr.response} nextArr={nextArr.response}/>
+      <div className='bg-slate-800 pt-20'>
+        <Post data={data} />
+      </div>
+    </>
 )}
 
 
