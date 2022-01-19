ad61477321827afba5d9ae8cf172c5774861503f
Lykøs <lykos@lykss-mbp.lan>
2022-01-19 11:13:09 -0800

    google auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import Navigation from '../components/Navigation'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps:{ session, ...pageProps }}: AppProps) {
 
   return (
-    <>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 const Home: NextPage = () => {
   return (
     <div className='bg-slate-800 h-screen w-screen flex flex-col items-center justify-center text-white'>
+
       <Link href='/MetaPathos/1'>
         Meta
       </Link>
