cf60e81895c7795cbbdfa6a655187ea7e22d348c
Lykøs <lykos@lykss-mbp.lan>
2022-01-26 00:59:57 -0800

    infinite nav progress

--- components/InfiniteNavigation.tsx
+++ components/InfiniteNavigation.tsx
@@ -1,15 +1,23 @@
 
+import { useRouter } from 'next/router'
 import HomeIcon from './HomeIcon'
 import InfiniteUserSearch from './InfiniteNavigation/InfiniteUserSearch'
+import Link from 'next/link'
 
 const InfiniteNavigation = () => {
 
+  const router = useRouter()
+  const { User } = router.query
+
   return (
   <div className='fixed top-0 left-0 right-0 z-10 bg-stone-900 text-white'>
-    <nav className='w-full h-20 z-20 md:flex justify-center items-center text-center place-content-around grid grid-cols-3'>
+    <nav className='w-full h-20 z-20 justify-center items-center text-center place-content-around grid grid-cols-3 md:grid-cols-5'>
       {/*<Image width={80} height={80} src='/headpats.gif' className='absolute top-0 left-0 right-0'/>*/}
       <HomeIcon />
-      <InfiniteUserSearch />     
+      <div></div>
+      <InfiniteUserSearch />
+      <div></div>
+      <Link href={'/' + User + '/1'}> Page Mode </Link>
     </nav>
   </div>
 )
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,18 +1,22 @@
 import NavButton from './Navigation/NavButton'
 import AlbumSearchUser from './AlbumUserSearch'
 import HomeIcon from './HomeIcon'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Navigation = () => {
-
+  const router = useRouter()
+  const { User } = router.query
   return (
   <div className='fixed top-0 left-0 right-0 z-10 bg-stone-900 text-white'>
     {/*<HomeIcon />*/}
-    <nav className='w-full h-20 z-20 md:flex justify-center items-center text-center place-content-around grid grid-cols-3'>
+    <nav className='w-full h-20 z-20 justify-center items-center text-center place-content-around grid grid-cols-3 md:grid-cols-5'>
       {/*<Image width={80} height={80} src='/headpats.gif' className='absolute top-0 left-0 right-0'/>*/}
-      <NavButton direction={false} />
       <HomeIcon />
+      <NavButton direction={false} />
       <AlbumSearchUser />     
       <NavButton direction={true} />
+      <Link href={'/' + User}> Infinite </Link>
     </nav>
   </div>
 )
