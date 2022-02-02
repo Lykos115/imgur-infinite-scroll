862b3f696efed1bbd9a22801c96f963d4608212e
Lykøs <lykos@lykss-mbp.lan>
2022-02-01 18:44:53 -0800

    adjusted css

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       {/*<Link href='/MetaPathos/1'>
         Meta
       </Link>*/}
-      <img src='/headpats.gif' className='w-24 h-24 rounded-xl' width={400} height={400}/>
+      <img src='/headpats.png' className='w-24 h-24 rounded-xl' onMouseOver={e => e.currentTarget.src = '/headpats.gif'} onMouseOut={e => e.currentTarget.src = '/headpats.png'} width={400} height={400}/>
       <div className='font-black text-6xl'>Headpats</div>
       <SearchUser />
     </div>
