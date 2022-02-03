25aa84a3539375b325f12bbf6e2a8bc2a3d3bf81
Lykøs <lykos@lykss-mbp.lan>
2022-02-02 21:16:32 -0800

    on hover head pats

--- components/HomeIcon.tsx
+++ components/HomeIcon.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link'
 const HomeIcon = () =>(
   <div className='justify-self-center md:order-none order-2 hover:cursor-pointer'>
     <Link href='/'>
-      <img src='/headpats.gif' alt='icon' className='w-16 h-16 rounded-xl justify-self-center md:justify-self-start' width={400} height={400}/>
+      <img src='/headpats.png' alt='icon' 
+        className='w-16 h-16 rounded-xl justify-self-center md:justify-self-start' 
+        onMouseOver={e => e.currentTarget.src = '/headpats.gif'} onMouseOut={e => e.currentTarget.src = '/headpats.png'} width={400} height={400}/>
     </Link>
   </div>    
 )
--- pages/_error.tsx
+++ pages/_error.tsx
@@ -4,7 +4,10 @@ export default function Error() {
   return (
     <div className='bg-slate-800 h-screen w-screen flex flex-col justify-center items-center text-center'>
       <Link href='/'>
-        <img src='/headpats.gif' className='w-40 h-40 rounded-xl hover:cursor-pointer' width={400} height={400}/>
+        <img src='/headpats.png' className='w-40 h-40 rounded-xl hover:cursor-pointer' 
+          onMouseOver={e => e.currentTarget.src='/headpats.gif'}
+          onMouseOut={e => e.currentTarget.src='/headpats.png'}
+          width={400} height={400}/>
       </Link>
       <h1 className='text-9xl font-black text-white'>404</h1>
       <div className='text-6xl font-bold text-white'>Oh no it seems this content doesn't exist.</div>
